688dd6f5e62728accea00f833c2196fd64c218e8
Rajani Kant Raghav <82476053+mrraghav9211@users.noreply.github.com>
2022-03-06 18:03:17 +0530

    6-march-test

---
+++ 6-march-test/index.js
@@ -0,0 +1,106 @@
+//iphone11 brand js
+function slide(){
+	document.getElementById('img-detail').src = "./iphone11/iphone11.jpg"
+	}
+function slide1(){
+   document.getElementById('img-detail').src = "./iphone11/iphone11a.jpg"
+}
+function slide2(){
+   document.getElementById('img-detail').src = "./iphone11/iphone11b.jpg"
+}
+function slide3(){
+   document.getElementById('img-detail').src = "./iphone11/iphone11c.jpg"
+}	
+ function show(){
+ 	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+    console.log(disp)
+ }
+ function hide(){
+ 	var disp = document.getElementsByClassName('details')[0];
+ 	 disp.classList.add('d-none')
+ }
+
+ //iphone13 pro brand js
+function show1(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./iphone13pro/iphone13pro.jpg"
+	document.getElementById('h2').innerText="Apple iPhone 13 Pro Max (256GB) - Sierra Blue"
+	document.getElementById('h6').innerText="Size name:256GB"
+	document.getElementById('h61').innerText="Colour: Sierra Blue"
+	document.getElementById('h62').innerText="Pattern name:iPhone 13 pro max"
+
+}
+function show2(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./iphone12/iphone12.jpg"
+	document.getElementById('h2').innerText="Apple iPhone 12 (128GB) - Blue"
+	document.getElementById('h6').innerText="Size name:128GB"
+	document.getElementById('h61').innerText="Colour:  Blue"
+	document.getElementById('h62').innerText="Pattern name:iPhone 12"
+
+}
+function show3(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./samsung/samsung1.jpg"
+	document.getElementById('h2').innerText="Samsung Galaxy M32 5G (Sky Blue, 8GB RAM, 128GB Storage)"
+	document.getElementById('h6').innerText="Size name:8, 128GB"
+	document.getElementById('h61').innerText="Colour:  Black"
+	document.getElementById('h62').innerText="Pattern name:Samsung M32 5G"
+
+}
+function show4(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./samsung/samsung2.jpg"
+	document.getElementById('h2').innerText="Samsung Galaxy M12 (Blue,6GB RAM, 128GB Storage)"
+	document.getElementById('h6').innerText="Size name: 6, 128GB"
+	document.getElementById('h61').innerText="Colour:  Blue"
+	document.getElementById('h62').innerText="Pattern name:Samsung M12"
+
+}
+
+function show5(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./vivo/vivo1.jpg"
+	document.getElementById('h2').innerText="Vivo V23 Pro 5G (Sunshine Gold, 8GB RAM, 128GB Storage)"
+	document.getElementById('h6').innerText="Size name: 8, 128GB"
+	document.getElementById('h61').innerText="Colour:  Sunshine Gold"
+	document.getElementById('h62').innerText="Pattern name:Vivo V23 Pro 5G"
+
+}
+
+function show6(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./vivo/vivo2.jpg"
+	document.getElementById('h2').innerText="Vivo V21e 5G (Sunset Jazz, 8GB RAM, 128GB Storage)"
+	document.getElementById('h6').innerText="Size name: 8, 128GB"
+	document.getElementById('h61').innerText="Colour:  Sunset Jazz"
+	document.getElementById('h62').innerText="Pattern name:Vivo V21e 5G"
+
+}
+function show7(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./mi/mi1.jpg"
+	document.getElementById('h2').innerText="Redmi 9 Power (Fiery Red, 4GB RAM, 64GB Storage)"
+	document.getElementById('h6').innerText="Size name: 8, 128GB"
+	document.getElementById('h61').innerText="Colour:  Fiery Red"
+	document.getElementById('h62').innerText="Pattern name: Redmi 9 Power"
+
+}
+function show8(){
+	var disp = document.getElementsByClassName('details')[0];
+ 	disp.classList.remove('d-none')
+	document.getElementById('img-detail').src = "./mi/mi2.jpg"
+	document.getElementById('h2').innerText="Mi 11X Pro 5G (Lunar White, 8GB RAM, 128GB Storage)"
+	document.getElementById('h6').innerText="Size name: 8, 128GB"
+	document.getElementById('h61').innerText="Colour:  Lunar White"
+	document.getElementById('h62').innerText="Pattern name: Mi 11X Pro 5G"
+
+}
